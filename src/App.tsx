be4533aee80ff5d91f103e8b026b2d68c70af228
Ananya <justdiscussanything@gmail.com>
2025-05-23 03:01:43 +0530

    project setup and dark mode with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+
+import "./App.css";
+
+type Wallet = {
+  balance: number;
+  address: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [walletInfo, setWalletInfo] = useState<Wallet | null>(null);
+  const [isDarkMode, setIsDarkMode] = useState(false); //TODO: set this so it sets automatically on the user preference misc
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="parent bg-white dark:bg-zinc-800 w-full max-w-[1280px] box-border h-[100vh] flex flex-col items-center justify-center relative px-4">
+        <div
+          className="absolute w-2 h-2 right-10 top-2 select-none cursor-pointer "
+          onClick={() => {
+            document.body.classList.toggle("dark"), setIsDarkMode(!isDarkMode);
+          }}
+        >
+          {isDarkMode ? (
+            <LightModeIcon className="text-white" />
+          ) : (
+            <DarkModeIcon className="text-zinc-800" />
+          )}
+        </div>
+        {walletInfo ? (
+          "Wallet info"
+        ) : (
+          <div className="bg-blue-800 text-blue-100  px-3 py-1 rounded-sm active:scale-[0.99] duration-100 select-none cursor-pointer">
+            Connect Wallet
+            {/* TODO:show the button only when the wallet info is not present or when the wallet is not connected */}
+          </div>
+        )}
+        <div></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
